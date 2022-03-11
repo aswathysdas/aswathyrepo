d8a5ef585e507f54db665f7e89c07e5a75bedf72
ASWATHY S DAS <aswathysdas43@gmail.com>
2022-03-11 18:37:22 +0530

    config.php

--- clientsidescripting/Mobilestore/application/config/config.php
+++ clientsidescripting/Mobilestore/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://php.trainees.baabtra.com/Mobilestore/';
+$config['base_url'] = 'http://aswathysdas/aswathysdasrepo/';
 
 /*
 |--------------------------------------------------------------------------
